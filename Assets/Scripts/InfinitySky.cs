7df0ccc67eec9ccfd35a70436616cbd1b0a99b5a
vband <vtor.barcellos7@gmail.com>
2017-04-28 19:51:33 -0300

    script melhorado

--- Assets/Scripts/InfinitySky.cs
+++ Assets/Scripts/InfinitySky.cs
@@ -7,29 +7,19 @@ public class InfinitySky : MonoBehaviour
 
     //gameobjects usados como referencia
     public GameObject player;
-    //public GameObject sky;
-    public GameObject sky;
 
     //prefabs para clonar
     public Transform skypf;
-    //vetor unitario para calculo das distancias
-    //private Vector3 unitario = new Vector3(1, 0, 0);
     //variaveis que armazeram a posicao xy do ultimo prefab clonado
     private Vector3 currentPos;
     private float skyWidth;
-    private Vector3 unitario = new Vector3(1, 0, 0);
-    //variaveis que armazeram a posicao xy do ultimo prefab clonado
-    private Vector3 lastpos_sky;
 
     void Start()
     {
         //inicializa as variaveis com os prefabs do inicio
         currentPos = skypf.transform.position;
-        //Player = GameObject.FindWithTag("Player");
         //Obtém o tamanho da sprite, em coordenadas de mundo
         skyWidth = skypf.GetComponent<SpriteRenderer>().bounds.size.x;
-        lastpos_sky = sky.transform.position;
-        //Player = GameObject.FindWithTag("Player");
     }
 
 
@@ -40,13 +30,6 @@ public class InfinitySky : MonoBehaviour
         {
             currentPos += skyWidth * Vector3.right;
             Instantiate(skypf, currentPos, new Quaternion(0, 0, 0, 0));
-            //lastpos_sky += (skyWidth * Vector3.right);
-            if (Vector3.Distance(player.transform.position, lastpos_sky) > 1 && Vector3.Distance(player.transform.position, lastpos_sky) < 50)
-            {
-                Instantiate(skypf, (lastpos_sky + 20.48f * unitario), new Quaternion(0, 0, 0, 0));
-                lastpos_sky += (20.48f * unitario);
-
-            }
         }
     }
 }
