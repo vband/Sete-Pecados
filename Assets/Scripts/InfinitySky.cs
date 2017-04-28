ee0b6503480a31eaa1f7ee5fa511b50edc3819bc
vband <vtor.barcellos7@gmail.com>
2017-04-28 19:18:22 -0300

    criação de prédios

---
+++ Assets/Scripts/InfinitySky.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfinitySky : MonoBehaviour {
+
+    //gameobjects usados como referencia
+    public GameObject player;
+    //public GameObject sky;
+   
+    //prefabs para clonar
+    public Transform skypf;
+    //vetor unitario para calculo das distancias
+    //private Vector3 unitario = new Vector3(1, 0, 0);
+    //variaveis que armazeram a posicao xy do ultimo prefab clonado
+    private Vector3 currentPos;
+    private float skyWidth;
+
+    void Start()
+    {
+        //inicializa as variaveis com os prefabs do inicio
+        currentPos = skypf.transform.position;
+        //Player = GameObject.FindWithTag("Player");
+        //Obtém o tamanho da sprite, em coordenadas de mundo
+        skyWidth = skypf.GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+
+    void Update()
+    {
+        //condicionais testam a distancia do ultimo prefab gerado e instancia os novos com base na distancia, garantindo q isso aconteca fora da visao do jogador
+        if (Vector3.Distance(player.transform.position, currentPos) < 50)
+        {
+            currentPos += skyWidth * Vector3.right;
+            Instantiate(skypf, currentPos, new Quaternion(0, 0, 0, 0));
+            //lastpos_sky += (skyWidth * Vector3.right);
+        }
+    }
+}
