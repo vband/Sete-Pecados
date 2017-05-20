9d5f88f60df7cb79304e9f0b8670ef7a26a72a49
vband <vtor.barcellos7@gmail.com>
2017-05-20 17:42:25 -0300

    Inimigo Ira: IA aprimorada

--- Assets/Scripts/MainGame/EnemyIraController.cs
+++ Assets/Scripts/MainGame/EnemyIraController.cs
@@ -9,41 +9,101 @@ public class EnemyIraController : MonoBehaviour
     public float speed;
     public float jumpForce;
     public float jumpCooldown;
+    public float chaseDistance;
+    public float idleDistance;
 
     private Transform player;
     private Rigidbody2D rb2D;
     private SpriteRenderer sprite;
-    private float currentCooldown;
+    private float currentJumpCooldown;
+    private float originPosition;
+    private int currentState;
+    private float distanceToPlayer, distanceToOrigin;
+    private bool isIdlingRight, isIdlingLeft;
+
+    private const int CHASE = 1;
+    private const int IDLE = 2;
+    private const int RETURN = 3;
 
     void Start ()
     {
         player = GameObject.Find("Player").transform;
         rb2D = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
-        currentCooldown = 0;
+        currentJumpCooldown = 0;
+        originPosition = transform.position.x;
+        currentState = IDLE;
+        isIdlingRight = true;
+        isIdlingLeft = false;
     }
 	
 	void Update ()
     {
         if (Time.timeScale != 0 && !SceneController.paused) //evita que atualize posicao enquanto jogo estiver pausado
         {
-            Move();
-
-            // Verifica se o player está acima e se existe umaplataforma dobre o inimigo
-            if ((player.position.y > transform.position.y + 2) && IsBelowPlatform())
+            // Diminui o cooldown de pulo
+            if (currentJumpCooldown > 0)
             {
-                Jump();
+                currentJumpCooldown -= Time.deltaTime;
             }
 
-            // Diminui o cooldown de pulo
-            if (currentCooldown > 0)
+            // Atualiza o estado do inimigo baseado na sua posição e na posição do jogador
+            UpdateState();
+
+            // Realiza a sua ação dependendo do estado atual
+            switch(currentState)
             {
-                currentCooldown -= Time.deltaTime;
+                // Se o inimigo estiver perseguindo o jogador...
+                case CHASE:
+                    // Corre atrás do jogador
+                    MoveToPlayer();
+
+                    // Verifica se o player está acima e se existe uma plataforma sobre o inimigo
+                    if ((player.position.y > transform.position.y + 2) && IsBelowPlatform())
+                    {
+                        Jump();
+                    }
+                    break;
+
+                // Se o inimigo estiver parado, sem haver ainda detectado o jogador...
+                case IDLE:
+                    // Move-se em volta do seu "spawn point", ocioso
+                    IdleAround();
+                    break;
+            }
+
+            // Se o inimigo ficar preso em algum obstáculo...
+            if (IsStuck())
+            {
+                // Pula, para tentar soltá-lo
+                Jump();
             }
         }
     }
 
-    private void Move()
+    // Atualiza o estado do inimigo baseado na sua posição e na posição do jogador
+    private void UpdateState()
+    {
+        // Calcula a distância entre o inimigo e o jogador
+        distanceToPlayer = Mathf.Abs(player.position.x - transform.position.x);
+        // Calcula a distância entre o inimigo e o seu ponto de origem
+        distanceToOrigin = Mathf.Abs(originPosition - transform.position.x);
+
+        // Se o jogador estiver dentro das vistas do inimigo...
+        if (distanceToPlayer <= chaseDistance)
+        {
+            // O inimigo perseguirá o jogador
+            currentState = CHASE;
+        }
+        // Senão, o inimigo fica ocioso
+        else
+        {
+            currentState = IDLE;
+        }
+    }
+
+    // Se desloca na direção do jogador
+    private void MoveToPlayer()
     {
         // Verifica se o player está para a direita
         if (player.position.x > transform.position.x)
@@ -61,28 +121,52 @@ public class EnemyIraController : MonoBehaviour
         }
     }
 
+    // Faz a ação de pular
     private void Jump()
     {
-        if ((currentCooldown <= 0) && IsGrounded())
+        if ((currentJumpCooldown <= 0) && IsGrounded())
         {
             rb2D.AddForce(Vector2.up * jumpForce);
-            currentCooldown = jumpCooldown;
+            currentJumpCooldown = jumpCooldown;
         }
     }
 
-    private bool IsBelowPlatform()
+    // Move-se em volta do seu "spawn point", ocioso
+    private void IdleAround()
     {
-        GetComponent<BoxCollider2D>().enabled = false;
-        RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position, Vector2.up, 2f);
-        GetComponent<BoxCollider2D>().enabled = true;
-
-        if (hit.collider != null && hit.collider.gameObject.layer == LayerMask.NameToLayer("Environment"))
+        // Atualiza o estado de se mover para a esquerda ou direita
+        if (isIdlingRight)
         {
-            return true;
+            if (transform.position.x > originPosition + idleDistance)
+            {
+                isIdlingRight = false;
+                isIdlingLeft = true;
+            }
         }
-        return false;
-    }
+        else if (isIdlingLeft)
+        {
+            if (transform.position.x < originPosition - idleDistance)
+            {
+                isIdlingRight = true;
+                isIdlingLeft = false;
+            }
+        }
 
+        // Move-se
+        if (isIdlingRight)
+        {
+            // Anda para a direita
+            rb2D.AddForce(Vector2.right * speed);
+            sprite.flipX = false;
+        }
+        else if (isIdlingLeft)
+        {
+            // Anda para a esquerda
+            rb2D.AddForce(Vector2.left * speed);
+            sprite.flipX = true;
+        }
+    }
+    
     // Essa função é chamada quando o inimigo colidir
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -103,6 +187,7 @@ public class EnemyIraController : MonoBehaviour
         }
     }
 
+    // Retorna true se o inimigo estiver tocando o chão
     private bool IsGrounded()
     {
         GetComponent<BoxCollider2D>().enabled = false;
@@ -116,4 +201,27 @@ public class EnemyIraController : MonoBehaviour
         return false;
     }
 
+    // Retorna true quando existe uma plataforma sobre o inimigo
+    private bool IsBelowPlatform()
+    {
+        GetComponent<BoxCollider2D>().enabled = false;
+        RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position, Vector2.up, 2f);
+        GetComponent<BoxCollider2D>().enabled = true;
+
+        if (hit.collider != null && hit.collider.gameObject.layer == LayerMask.NameToLayer("Environment"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    // Returna true se o inimigo estiver parado
+    private bool IsStuck()
+    {
+        if (GetComponent<Rigidbody2D>().velocity.x == 0)
+        {
+            return true;
+        }
+        return false;
+    }
 }
