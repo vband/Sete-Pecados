2811a821afe9933f00d8083ba4fd3ae7d63ddb78
Alonso-PC\Alonso <alonsoaman@hotmail.com>
2017-05-22 22:30:32 -0300

    PowerUpAguaBentaComParticulas

--- Assets/Scripts/MainGame/aguaBentaPowerUp.cs
+++ Assets/Scripts/MainGame/aguaBentaPowerUp.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class aguaBentaPowerUp : MonoBehaviour {
 
+    public float AutoDestroyTimer;
+
 	// Use this for initialization
 	void Start () {
         Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Environment"), LayerMask.NameToLayer("AguaBentaPowerUp"));
-        
     }
 	
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
         if(collision.gameObject.layer == LayerMask.NameToLayer("Enemies"))
         {
             
             Destroy(collision.gameObject);
             Destroy(gameObject);
         }
+    }
 
+    //sem uso, mas pode ser uma possivel implementacao mais elegante
+    void AutoDestroyStart(float time)
+    {
+        AutoDestroy(time);
+    }
+
+    IEnumerator AutoDestroy(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(this.gameObject);
     }
 }
