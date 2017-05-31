ea15d591e1be2a4c44dac0d072aa81596bdcea53
vband <vtor.barcellos7@gmail.com>
2017-05-30 21:51:08 -0300

    Final da fase: Protótipo

---
+++ Assets/Scripts/MainGame/IgrejaBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgrejaBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Venceu o jogo!!");
+            GameObject.Find("FadeImage").GetComponent<FadeController>().CallFading("GameOver");
+        }
+    }
+}
--- Assets/Scripts/MainGame/InfiniteWorld.cs
+++ Assets/Scripts/MainGame/InfiniteWorld.cs
@@ -7,24 +7,46 @@ public class InfiniteWorld : MonoBehaviour
     public Transform player;
     public Transform[] objectsToGenerate; // Array de objetos que serão gerados constantemente: prédios, postes, etc
     public Transform beginnigOfWorld, endOfWorld; // Ponteiros para o início e o fim da cidade no momento inicial do jogo
+    public SceneController cenaPrincipal;
+    public Transform igrejaPrefab;
 
     private float worldWidth; // Largura inicial da cidade
     private int nIterations; // Quantidade atual de vezes que o script já gerou novos objetos
+    private Vector3 currentCityEndPos;
+    private float distanceToWin;
+    private bool hasCreatedChurch;
 
 	void Start ()
     {
         //Calcula a largura inicial da cidade
         worldWidth = endOfWorld.position.x - beginnigOfWorld.position.x;
         nIterations = 1;
-	}
-	
-	void Update ()
+        distanceToWin = cenaPrincipal.distanceToWin;
+        hasCreatedChurch = false;
+    }
+
+    void Update ()
     {
+        // Atualiza a posição do final da cidade
+        currentCityEndPos = endOfWorld.position + Vector3.right * worldWidth * (nIterations-1);
+
+        // Se a cidade já cresceu o bastante para terminar o jogo, cria a igreja
+        if (Vector3.Distance(beginnigOfWorld.position, currentCityEndPos) >= distanceToWin)
+        {
+            if (!hasCreatedChurch)
+            {
+                CreateChurch();
+                hasCreatedChurch = true;
+            }
+        }
+        
         // Se o jogador se aproximar do fim da cidade, gera novos objetos
-		if (Vector3.Distance(player.position, endOfWorld.position + Vector3.right * worldWidth * nIterations) < 50)
+        else if (Vector3.Distance(player.position, currentCityEndPos) < 50)
         {
             generateMoreObjects();
         }
+
+        Debug.DrawLine(beginnigOfWorld.position, currentCityEndPos, Color.red);
 	}
 
     private void generateMoreObjects()
@@ -38,5 +60,18 @@ public class InfiniteWorld : MonoBehaviour
         }
 
         nIterations++;
+    }
+
+    private void CreateChurch()
+    {
+        Transform instance = Instantiate(igrejaPrefab,
+            igrejaPrefab.position + new Vector3(currentCityEndPos.x, 0, 0),
+            new Quaternion(0, 0, 0, 0),
+            cenaPrincipal.transform);
+
+        //Debug.Log(instance.GetComponent<BoxCollider2D>().bounds.extents.x);
+
+        float igrejaExtent = instance.GetComponent<BoxCollider2D>().bounds.extents.x;
+        instance.position = new Vector3 (instance.position.x + igrejaExtent + 1.75f, instance.position.y, instance.position.z);
     }
 }
