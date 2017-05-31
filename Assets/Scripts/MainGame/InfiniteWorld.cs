0982811e83c1b07de90eb25d91ff1ca146bd4d9c
vband <vtor.barcellos7@gmail.com>
2017-05-31 20:33:00 -0300

    Script de geração da cidade adaptado para gerar segmentos diferentes aleatoriamente

--- Assets/Scripts/MainGame/InfiniteWorld.cs
+++ Assets/Scripts/MainGame/InfiniteWorld.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class InfiniteWorld : MonoBehaviour
 {
     public Transform player;
-    public Transform[] objectsToGenerate; // Array de objetos que serão gerados constantemente: prédios, postes, etc
+    public Transform[] objectsToGenerate1; // 1o array de objetos que serão gerados
+    public Transform[] objectsToGenerate2; // 2o array de objetos que serão gerados
+    public Transform[] objectsToGenerate3; // 3o array de objetos que serão gerados
     public Transform beginnigOfWorld, endOfWorld; // Ponteiros para o início e o fim da cidade no momento inicial do jogo
     public SceneController cenaPrincipal;
     public Transform igrejaPrefab;
@@ -15,6 +17,8 @@ public class InfiniteWorld : MonoBehaviour
     private Vector3 currentCityEndPos;
     private float distanceToWin;
     private bool hasCreatedChurch;
+    private int arrayPointer; // Variável que guarda qual array de objetos está sendo usado para criar a cidade
+    private List<int> arrayIndexes; // Os índices dos arrays de objetos: 1, 2 ou 3
 
 	void Start ()
     {
@@ -23,6 +27,8 @@ public class InfiniteWorld : MonoBehaviour
         nIterations = 1;
         distanceToWin = cenaPrincipal.distanceToWin;
         hasCreatedChurch = false;
+        arrayIndexes = new List<int>() {1, 2, 3};
+        arrayPointer = 1;
     }
 
     void Update ()
@@ -50,14 +56,47 @@ public class InfiniteWorld : MonoBehaviour
     private void generateMoreObjects()
     {
         // Instancia todos os objetos que foram preparados antecipadamente
-        for (int i = 0; i < objectsToGenerate.Length; i++)
+        switch(arrayPointer)
         {
-            Instantiate(objectsToGenerate[i], objectsToGenerate[i].position + Vector3.right * worldWidth * nIterations, 
-                        new Quaternion(0, 0, 0, 0), this.transform);
-            
+            case 1:
+                for (int i = 0; i < objectsToGenerate1.Length; i++)
+                {
+                    Instantiate(objectsToGenerate1[i], objectsToGenerate1[i].position + Vector3.right * worldWidth * nIterations,
+                                new Quaternion(0, 0, 0, 0), this.transform);
+
+                }
+                break;
+
+            case 2:
+                for (int i = 0; i < objectsToGenerate2.Length; i++)
+                {
+                    Instantiate(objectsToGenerate2[i], objectsToGenerate2[i].position + Vector3.right * worldWidth * nIterations,
+                                new Quaternion(0, 0, 0, 0), this.transform);
+
+                }
+                break;
+
+            case 3:
+                for (int i = 0; i < objectsToGenerate3.Length; i++)
+                {
+                    Instantiate(objectsToGenerate3[i], objectsToGenerate3[i].position + Vector3.right * worldWidth * nIterations,
+                                new Quaternion(0, 0, 0, 0), this.transform);
+
+                }
+                break;
         }
 
         nIterations++;
+
+        // Muda o array que será usado na próxima vez
+        // Anota qual foi o último usado, pois esse não poderá repetir
+        int lastIndex = arrayPointer;
+        // Remove o último usado da lista
+        arrayIndexes.Remove(lastIndex);
+        // Sorteia um índice novo, dentre os que não foram usados na última vez
+        arrayPointer = arrayIndexes[Random.Range(0, arrayIndexes.Count)];
+        // Devolve o índice que tinha sido removido à lista
+        arrayIndexes.Add(lastIndex);
     }
 
     private void CreateChurch()
