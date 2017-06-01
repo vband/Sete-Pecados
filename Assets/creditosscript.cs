a6bc81d68377af31efb5d6a6d531441f5a7c995c
Alonso-PC\Alonso <alonsoaman@hotmail.com>
2017-06-01 00:00:25 -0300

    final

---
+++ Assets/creditosscript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class creditosscript : MonoBehaviour {
+
+    public GameObject TelaInicial,opcoes,creditos;
+    
+
+    public void clickcreditos()
+    {
+        TelaInicial.SetActive(false);
+        opcoes.SetActive(false);
+        creditos.SetActive(true);
+    }
+
+    public void clickvoltar()
+    {
+        TelaInicial.SetActive(true);
+        opcoes.SetActive(false);
+        creditos.SetActive(false);
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
