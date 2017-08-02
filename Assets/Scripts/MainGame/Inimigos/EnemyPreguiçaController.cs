223846e1e207b55e2f5f13e3525e43b37db4db53
vband <vtor.barcellos7@gmail.com>
2017-08-02 19:58:23 -0300

    Organização dos scripts: criado um script novo que gerencia todas as colisões do jogador

--- Assets/Scripts/MainGame/Inimigos/EnemyPreguiçaController.cs
+++ Assets/Scripts/MainGame/Inimigos/EnemyPreguiçaController.cs
@@ -4,37 +4,9 @@ using UnityEngine;
 
 public class EnemyPreguiçaController : MonoBehaviour
 {
-    private FadeController FadeImage;
-
     private void Start()
     {
         //debug para inimigo da preguiça spawnar deitado
         transform.Rotate(Vector3.forward, 90);
-        FadeImage = GameObject.Find("FadeImage").GetComponent<FadeController>();
-    }
-
-    // Essa função é chamada quando o inimigo colidir
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        // Se o inimigo colidir com o jogador
-        if (collision.gameObject.tag == "Player")
-        {
-            GameObject player = collision.gameObject;
-
-            // Se o jogador estava invulnerável
-            if (player.GetComponent<PlayerMovement>().isImortal())
-            {
-                player.GetComponent<PlayerMovement>().sobeCarinha();
-                goto Destruir;
-            }
-                        
-            player.GetComponent<Animator>().enabled = false;
-
-            FadeImage.FadeFromColision("Preguiça", transform.position, FadeController.PREGUICA);
-            
-
-            Destruir:
-                Destroy(gameObject);
-        }
     }
 }
