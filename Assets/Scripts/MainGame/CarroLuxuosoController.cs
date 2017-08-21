38aa327f877feed7deeea74549ea3f7323284ca7
vband <vtor.barcellos7@gmail.com>
2017-08-21 20:06:34 -0300

    Iminigo Inveja implementado com placeholders

---
+++ Assets/Scripts/MainGame/CarroLuxuosoController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarroLuxuosoController : MonoBehaviour
+{
+    public float speed;
+
+    private PointEffector2D pointEffector;
+    private SpriteRenderer spriteRenderer;
+
+	void Start ()
+    {
+        pointEffector = GetComponent<PointEffector2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        pointEffector.enabled = false;
+	}
+	
+	void FixedUpdate ()
+    {
+        if (!SceneController.paused || SceneController.hasGameFinished)
+        {
+            if (spriteRenderer.isVisible)
+            {
+                Move();
+            }
+        }
+
+        DestroyNearbyCars();
+    }
+
+    private void Move()
+    {
+        GetComponent<Rigidbody2D>().MovePosition((Vector2)transform.position + Vector2.left * speed * Time.fixedDeltaTime);
+    }
+
+    private void DestroyNearbyCars()
+    {
+        RaycastHit2D leftHit = Physics2D.Raycast(transform.position, Vector2.left, 15f);
+        RaycastHit2D rightHit = Physics2D.Raycast(transform.position, Vector2.right, 15f);
+
+        if (leftHit.collider != null && leftHit.collider.gameObject.tag == "car")
+        {
+            Destroy(leftHit.collider.gameObject);
+        }
+
+        if (rightHit.collider != null && rightHit.collider.gameObject.tag == "car")
+        {
+            Destroy(rightHit.collider.gameObject);
+        }
+    }
+
+    public void OnInvejosoVisible()
+    {
+        pointEffector.enabled = true;
+    }
+}
