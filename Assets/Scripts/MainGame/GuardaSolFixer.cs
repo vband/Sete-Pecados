b9b73c74347605487f02311b2b22a58180521045
vband <vtor.barcellos7@gmail.com>
2017-08-27 12:06:51 -0300

    Guarda-sol não se move mais no eixo x

---
+++ Assets/Scripts/MainGame/GuardaSolFixer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardaSolFixer : MonoBehaviour
+{
+    private Vector3 startPos;
+
+	void Start ()
+    {
+        startPos = transform.position;
+	}
+
+    private void FixedUpdate()
+    {
+        if (transform.position.x != startPos.x)
+        {
+            transform.position = new Vector3(startPos.x, transform.position.y, transform.position.z);
+        }
+    }
+}
