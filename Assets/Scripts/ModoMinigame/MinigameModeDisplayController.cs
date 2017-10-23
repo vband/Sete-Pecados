9a3d51d1ab04341548bdab8da1a8e7205f3aec7f
vband <vtor.barcellos7@gmail.com>
2017-10-23 12:38:24 -0200

    Modo Minigame passado para português

--- Assets/Scripts/ModoMinigame/MinigameModeDisplayController.cs
+++ Assets/Scripts/ModoMinigame/MinigameModeDisplayController.cs
@@ -11,8 +11,8 @@ public class MinigameModeDisplayController : MonoBehaviour
     public void UpdateDisplay(int newNumberOfLives, int newScore, int newHighScore)
     {
         lives.text = "x " + newNumberOfLives.ToString();
-        score.text = "Score: " + newScore.ToString();
-        highScore.text = "High Score: " + newHighScore.ToString();
+        score.text = "Pontos: " + newScore.ToString();
+        highScore.text = "Recorde: " + newHighScore.ToString();
 
 #if UNITY_ANDROID
         Screen.orientation = ScreenOrientation.LandscapeLeft;
