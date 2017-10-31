31b811938a8851c3344a2df37e1e2245454aca64
vband <vtor.barcellos7@gmail.com>
2017-10-31 20:03:59 -0200

    Inimigo Luxúria animado com as novas sprites

--- Assets/Scripts/MainGame/Inimigos/EnemyLuxuriaController.cs
+++ Assets/Scripts/MainGame/Inimigos/EnemyLuxuriaController.cs
@@ -6,9 +6,12 @@ public class EnemyLuxuriaController : MonoBehaviour {
 
     [SerializeField] private GameObject Arremecavel;
 
+    private Animator animator;
+
 	// Use this for initialization
 	void Start () {
-        StartCoroutine(Arremassador());
+        //StartCoroutine(Arremassador());
+        animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -16,13 +19,32 @@ public class EnemyLuxuriaController : MonoBehaviour {
        
 	}
 
+    /*
     IEnumerator Arremassador()
     {
         inicio:
-        yield return new WaitForSeconds(0.8f);
-        //tem chance de arremessar os objetos indecentes
-        if (Random.Range(0,100) > 50 && !SceneController.paused)
-            Instantiate(Arremecavel, transform.position + Vector3.left, Quaternion.identity);        
+            yield return new WaitForSeconds(0.8f);
+            //tem chance de arremessar os objetos indecentes
+            if (Random.Range(0,100) > 50 && !SceneController.paused)
+                //Instantiate(Arremecavel, transform.position + Vector3.left, Quaternion.identity);
+                
         goto inicio;
+    }
+    */
+
+    private void InstantiateBullet()
+    {
+        Instantiate(Arremecavel, transform.position + Vector3.left, Quaternion.identity);
+    }
+
+    private IEnumerator StopRandomly()
+    {
+        // Tem uma chance de 60% de pausar os arremessos de projéteis por 0.8 segundos
+        if (Random.Range(0f, 100f) < 60f)
+        {
+            animator.speed = 0;
+            yield return new WaitForSeconds(0.8f);
+            animator.speed = 1;
+        }
     }
 }
